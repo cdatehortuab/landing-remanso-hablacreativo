57f880bbeb9db4411fca29e8a62f38efed008f35
Cristian Atehortua <cristian25a32@gmail.com>
2018-09-03 11:34:32 -0500

    Hide label when input is empty in input-group component.

--- app/components/input-group.js
+++ app/components/input-group.js
@@ -1,5 +1,10 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { isEmpty } from '@ember/utils';
 
 export default Component.extend({
   classNames: ['input-group'],
+  showLabel: computed('value', function() {
+    return !isEmpty(this.get('value'));
+  }),
 });
